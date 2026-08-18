f858d2708b1de9c9c1164d5c8bb931909a3ffdbb
marym <marymcguire1718@gmail.com>
2026-08-18 17:44:22 +0200

    update formatting as per PR comment

--- wagtailyoast/static/wagtailyoast/src/js/yoastworker.js
+++ wagtailyoast/static/wagtailyoast/src/js/yoastworker.js
@@ -1,7 +1,7 @@
 import { AnalysisWebWorker } from 'yoastseo';
-import EnglishResearcher from "yoastseo/build/languageProcessing/languages/en/Researcher";
+import EnglishResearcher from 'yoastseo/build/languageProcessing/languages/en/Researcher';
 
 // Run Yoast Worker
 
-const worker = new AnalysisWebWorker(self, new EnglishResearcher() );
+const worker = new AnalysisWebWorker(self, new EnglishResearcher());
 worker.register();
