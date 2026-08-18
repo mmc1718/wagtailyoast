809f3a2224f705b6a862ea35e64428829e9ee46e
marym <marymcguire1718@gmail.com>
2026-08-18 15:04:42 +0200

    use emojis instead of relying of font awesome

--- wagtailyoast/static/wagtailyoast/src/js/common/ResultContainers.js
+++ wagtailyoast/static/wagtailyoast/src/js/common/ResultContainers.js
@@ -32,8 +32,8 @@ export default class ResultContainers {
    */
   static scoreIcon(result) {
     return ResultContainers.isSuccessResult(result)
-      ? '<i class="icon icon-tick"></i>'
-      : '<i class="icon icon-cross"></i>';
+      ? '<i class="icon">✅</i>'
+      : '<i class="icon">❗️</i>';
   }
 
 
