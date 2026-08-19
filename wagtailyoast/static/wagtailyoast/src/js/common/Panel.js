283627d8d733a4c06dbff5f4bed3a0ede1b78660
marym <marymcguire1718@gmail.com>
2026-08-19 21:38:49 +0200

    update preview url

--- wagtailyoast/static/wagtailyoast/src/js/common/Panel.js
+++ wagtailyoast/static/wagtailyoast/src/js/common/Panel.js
@@ -20,18 +20,7 @@ export default class Panel extends WithContext {
    * @returns {string}
    */
   static async getPreviewPageContent() {
-    const $form = $('#page-edit-form');
-    const $previewBtn = $('button[class^="button action-preview"');
-    const previewUrl = $previewBtn.data('action');
-
-    // Submit wagtail edit form with no processing data
-    await $.ajax({
-      url: previewUrl,
-      method: 'POST',
-      data: new FormData($form[0]),
-      processData: false,
-      contentType: false,
-    });
+    const previewUrl = `${window.location.pathname}preview/`
 
     // Get content of preview page
     const result = await $.ajax({
